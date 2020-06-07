a3e96856b13d1e9df4a675d824388006c185d914
Tâm Lê <tamle@teqnological.asia>
2020-06-07 23:20:03 +0700

    update hero section

---
+++ src/components/Top/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from 'src/components/commons/HeroSection';
+
+interface Props {}
+
+const Top: React.FC<Props> = ({}) => {
+  return (
+  <div>
+    <HeroSection />
+  </div>
+  );
+};
+
+export default Top;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import "src/styles/_all.scss";
-import Myfunc from "src/components/example";
-import Login from "src/components/Login";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { About } from "src/components/About";
-import Header from 'src/components/commons/Header';
-import Footer from 'src/components/commons/Footer';
+import Header from "src/components/commons/Header";
+import Footer from "src/components/commons/Footer";
+
+import Myfunc from "src/components/example";
+import Login from "src/components/Login";
+import Top from "src/components/Top";
 
 function App() {
   return (
     <Router>
       <Header />
       <Switch>
+        <Route path="/">
+          <Top />
+        </Route>
         <Route path="/example">
           <Myfunc title="HUHU" />
         </Route>
