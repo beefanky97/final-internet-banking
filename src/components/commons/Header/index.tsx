5e5f18a3241a9b5c45f27890c9bf1c7a46931e3c
Tâm Lê <tamle@teqnological.asia>
2020-06-08 23:47:00 +0700

    update link header

--- src/components/commons/Header/index.tsx
+++ src/components/commons/Header/index.tsx
@@ -77,10 +77,10 @@ const Header: React.FC<Props> = ({}) => {
                   <div className="classynav">
                     <ul>
                       <li>
-                        <a href="index.html">Home</a>
+                        <a href="./">Home</a>
                       </li>
                       <li>
-                        <a href="about.html">About Us</a>
+                        <a href="./about">About Us</a>
                       </li>
                       <li>
                         <a href="#">Pages</a>
@@ -109,7 +109,7 @@ const Header: React.FC<Props> = ({}) => {
                         </ul>
                       </li>
                       <li>
-                        <a href="services.html">Services</a>
+                        <a href="./login">Login</a>
                       </li>
                       <li>
                         <a href="#">Portfolio</a>
@@ -231,152 +231,6 @@ const Header: React.FC<Props> = ({}) => {
         </div>
       </header>
       {/* <!-- ##### Header Area End ##### --> */}
-{/* 
-      <div className="hero-area">
-        <div className="hero-slideshow owl-carousel">
-          <div className="single-slide bg-img">
-            <img
-              className="slide-bg-img bg-img bg-overlay"
-              src="img/bg-img/1.jpg"
-              alt=""
-            />
-            <div className="container h-100">
-              <div className="row h-100 align-items-center justify-content-center">
-                <div className="col-12 col-lg-9">
-                  <div className="welcome-text text-center">
-                    <h6 data-animation="fadeInUp" data-delay="100ms">
-                      2 years interest
-                    </h6>
-                    <h2 data-animation="fadeInUp" data-delay="300ms">
-                      get your <span>loan</span> now
-                    </h2>
-                    <p data-animation="fadeInUp" data-delay="500ms">
-                      Vestibulum eu vehicula elit, nec elementum orci. Praesent
-                      aliquet ves tibulum tempus. Pellentesque posuere pharetra
-                      turpis, eget finibus erat porta placerat.
-                    </p>
-                    <a
-                      href="#"
-                      className="btn credit-btn mt-50"
-                      data-animation="fadeInUp"
-                      data-delay="700ms"
-                    >
-                      Discover
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="slide-du-indicator"></div>
-          </div>
-
-          <div className="single-slide bg-img">
-            <img
-              className="slide-bg-img bg-img bg-overlay"
-              src="img/bg-img/1.jpg"
-              alt=""
-            />
-            <div className="container h-100">
-              <div className="row h-100 align-items-center justify-content-center">
-                <div className="col-12 col-lg-9">
-                  <div className="welcome-text text-center">
-                    <h6 data-animation="fadeInDown" data-delay="100ms">
-                      2 years interest
-                    </h6>
-                    <h2 data-animation="fadeInDown" data-delay="300ms">
-                      get your <span>loan</span> now
-                    </h2>
-                    <p data-animation="fadeInDown" data-delay="500ms">
-                      Vestibulum eu vehicula elit, nec elementum orci. Praesent
-                      aliquet ves tibulum tempus. Pellentesque posuere pharetra
-                      turpis, eget finibus erat porta placerat.
-                    </p>
-                    <a
-                      href="#"
-                      className="btn credit-btn mt-50"
-                      data-animation="fadeInDown"
-                      data-delay="700ms"
-                    >
-                      Discover
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="slide-du-indicator"></div>
-          </div>
-
-          <div className="single-slide bg-img">
-            <img
-              className="slide-bg-img bg-img bg-overlay"
-              src="img/bg-img/5.jpg"
-            />
-            <div className="container h-100">
-              <div className="row h-100 align-items-center justify-content-center">
-                <div className="col-12 col-lg-9">
-                  <div className="welcome-text text-center">
-                    <h6 data-animation="fadeInUp" data-delay="100ms">
-                      2 years interest
-                    </h6>
-                    <h2 data-animation="fadeInUp" data-delay="300ms">
-                      get your <span>loan</span> now
-                    </h2>
-                    <p data-animation="fadeInUp" data-delay="500ms">
-                      Vestibulum eu vehicula elit, nec elementum orci. Praesent
-                      aliquet ves tibulum tempus. Pellentesque posuere pharetra
-                      turpis, eget finibus erat porta placerat.
-                    </p>
-                    <a
-                      href="#"
-                      className="btn credit-btn mt-50"
-                      data-animation="fadeInUp"
-                      data-delay="700ms"
-                    >
-                      Discover
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="slide-du-indicator"></div>
-          </div>
-
-          <div className="single-slide bg-img">
-            <img
-              className="slide-bg-img bg-img bg-overlay"
-              src="img/bg-img/5.jpg"
-            />
-            <div className="container h-100">
-              <div className="row h-100 align-items-center justify-content-center">
-                <div className="col-12 col-lg-9">
-                  <div className="welcome-text text-center">
-                    <h6 data-animation="fadeInDown" data-delay="100ms">
-                      2 years interest
-                    </h6>
-                    <h2 data-animation="fadeInDown" data-delay="300ms">
-                      get your <span>loan</span> now
-                    </h2>
-                    <p data-animation="fadeInDown" data-delay="500ms">
-                      Vestibulum eu vehicula elit, nec elementum orci. Praesent
-                      aliquet ves tibulum tempus. Pellentesque posuere pharetra
-                      turpis, eget finibus erat porta placerat.
-                    </p>
-                    <a
-                      href="#"
-                      className="btn credit-btn mt-50"
-                      data-animation="fadeInDown"
-                      data-delay="700ms"
-                    >
-                      Discover
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="slide-du-indicator"></div>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
