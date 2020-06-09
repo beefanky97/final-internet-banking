159a809419d728a0f6ef8a935b0eac8755f95c81
Tâm Lê <tamle@teqnological.asia>
2020-06-09 11:56:35 +0700

    logic check expired token

--- src/components/utils/functions.tsx
+++ src/components/utils/functions.tsx
@@ -5,7 +5,7 @@ export const isValidToken = () => {
   if (token_expire && +token_expire) {
     const currentTime = new Date().getTime();
     const expireTime = +token_expire * 1000 - 30000; // before real expire time on server 30s
-    return expireTime < currentTime;
+    return expireTime >= currentTime;
   }
   //set to using
   return false;
