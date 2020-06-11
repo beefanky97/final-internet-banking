868313ee1408453535c05761ffbeceb3261f8354
Tâm Lê <tamle@teqnological.asia>
2020-06-11 21:42:47 +0700

    turn on auth api

--- src/api/accountService.tsx
+++ src/api/accountService.tsx
@@ -1,21 +1,21 @@
-// import { appAxios } from "./appAxios";
-// import { AxiosResponse, AxiosError } from "axios";
+import { appAxios } from "./appAxios";
+import { AxiosResponse, AxiosError } from "axios";
 
 export const accountService = {
   login: async (username: string, password: string) => {
-    return {
-      data: {
-        access_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOiI5OTk5OTk5OTk5OTk5OSJ9.8BunVT1Hf3qO7E2Mbrnarsb79srJe_tXcBAczFnps4Q",
-        refresh_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOiI5OTk5OTk5OTk5OTk5OSJ9.8BunVT1Hf3qO7E2Mbrnarsb79srJe_tXcBAczFnps4Q"
-      },
-    };
-    // return await appAxios.post("/login", { username, password });
-    //   .then((res: AxiosResponse) => {
-    //     return res;
-    //   })
-    //   .catch((err: AxiosError) => {
-    //     console.log(err);
-    //   });
+    // return {
+    //   data: {
+    //     access_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOiI5OTk5OTk5OTk5OTk5OSJ9.8BunVT1Hf3qO7E2Mbrnarsb79srJe_tXcBAczFnps4Q",
+    //     refresh_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOiI5OTk5OTk5OTk5OTk5OSJ9.8BunVT1Hf3qO7E2Mbrnarsb79srJe_tXcBAczFnps4Q"
+    //   },
+    // };
+    return await appAxios.post("/auth", { username, password })
+      .then((res: AxiosResponse) => {
+        return res;
+      })
+      .catch((err: AxiosError) => {
+        console.log(err);
+      });
   },
   refreshToken: async (refresh_token: string) => {
     return {
